cff9b0e797682be1d4dc13c4dc8f418699c4e176
Lovsan <lovster6@gmail.com>
2020-07-29 19:35:44 +0300

    This is command.js file to show when round starts and ends, just an example.
    lets make this better and better...even bettter? automated! 👍

--- StartEnd.js
+++ StartEnd.js
@@ -1,15 +1,15 @@
 client.on("message", message => {
-  if (message.author.bot) return;
+  // if (message.author.bot) return; - commented out for now...
   // This is where we'll put our code.
   if (message.content.indexOf(config.prefix) !== 0) return;
  
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
  
-  if(command === 'round start') {
+  if(command === 'RoundStart') {
     message.channel.send('1.9.2020!');
   } else
-  if (command === 'round end') {
+  if (command === 'RoundEnd') {
     message.channel.send('29.8.2020!');
   }
 });
