bba212b557e6ac085288f484e3165654fef34e01
Lovsan <lovster6@gmail.com>
2020-07-29 19:39:17 +0300

    Update sklonebot.js

--- sklonebot.js
+++ sklonebot.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 const RoundStart = ("StartEnd.js");
 const client = new Discord.Client();
 const config = require("./config.json");
-const prefix = "!sklone"; // set prefix in json
+//const prefix = "!*"; // set prefix in json
 
 
 client.on("ready", () => {
@@ -19,12 +19,6 @@ client.on("message", (message) => {
   } else
   if (message.content.startsWith(prefix + "lovsan")) {
     message.channel.send("Lovsan IS the SHIT TOO!");
-  }
-  if (command === "asl") {
-    let age = args[0]; // Remember arrays are 0-based!.
-    let sex = args[1];
-    let location = args[2];
-    message.reply(`Wazzupp ${message.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
   }
   if(command === "say"){
     let text = args.join(" ");
